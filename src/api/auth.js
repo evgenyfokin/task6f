05e8dd2fdc6da1033e0a7f3cd5458aa9249f2c3d
evgenyfokin <fokin_e@inbox.ru>
2023-07-28 17:30:14 +0300

    fix

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// export const API_URL = 'http://localhost:3000'
 export const API_URL = 'https://nameless-wildwood-04003-8f80ca6edf3a.herokuapp.com'
 
 export const getMessages = (name) => axios.get(`${API_URL}/messages`)
